c19af8c9b9f8d96e1cc9c57e0fe6c5cf90eeb240
youssef456 <yousefnagy2@gmail.com>
2023-12-08 03:52:00 +0200

    test

---
+++ rtos-start-stop.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Path to the directory where your application is installed
+APP_DIR="/usr/bin/"
+
+# Command to run your application
+APP_COMMAND="rtos"
+
+# Run your application
+echo "Running RTOS..."
+${APP_DIR}${APP_COMMAND}
